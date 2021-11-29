64e164a4803eb4a503848e649b67a5ae21dc5432
Esra Alkan <es.alknn@gmail.com>
2021-11-29 17:29:47 +0300

    Add project files.

---
+++ MCD_KapsüllemeNedir/Musteri.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_KapsüllemeNedir
+{
+    class Musteri
+    {
+
+        //Class => Field
+        int id;
+        public string isim;
+        public string soyisim;
+
+        //Class => Property
+        private string emailAdres;
+        public string EmailAdres
+        {
+            //get;//Database veya farklı bir veri kaynağı class çağıran prodramcıya datayı gösterdiğimiz alan
+            //set;//Dış dünyadan alınan datanın içerisinde private olarak saklanan fiels içerisine değer atandığı kısım... 
+
+            set
+            {
+                this.emailAdres = value;
+            }
+            get
+            {
+                return this.emailAdres;
+            }
+        }
+    }
+}
---
+++ MCD_KapsüllemeNedir/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_KapsüllemeNedir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri M1 = new Musteri();
+            /*
+             * Field değer ataması yapılmasın ama değer okuması yapılabilsin.- private set {....}
+             * Field değer ataması yapılsın ama değer okuması yapılmasın.-private get {}
+             * Field değer ataması yapılsın ama değerin ilk 4 hanesini okusun.
+             * Field değer ataması ve okuma işlemleri yapılsın ama bu iki işlemde bizim belirlediğimiz bir süreçten geçsin veri daha sonra gösterilsin.
+             * 
+             */
+            //M1.emailAdres = "esra";(field)
+
+            //Genel olarak kapsülleme konusuna giriş yaptık.
+            M1.EmailAdres = "Esra Alkan";
+            Console.WriteLine(M1.EmailAdres);
+
+            Console.ReadKey();
+        }
+    }
+}
